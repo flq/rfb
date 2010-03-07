31f096a07f6c8ece8965025254fff42e7f741b40
flq <fquednau@gmail.com>
2010-03-07 21:58:15 +0100

    Improved AssemblyInfo

--- rfb/Properties/AssemblyInfo.cs
+++ rfb/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("rfb")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("realfiction builder")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("realfiction")]
 [assembly: AssemblyProduct("rfb")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © realfiction 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
